70a12228a56b5c83ef5c3db35587c7c8facdce72
starrett67 <jstarrett@3blades.io>
2019-03-07 20:47:31 -0500

    Fix icon and remove unused ones

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        icon: 'src/images/icons8-circled-j-50.png' // This path is relative to the root of the site.
       }
     },
     'gatsby-plugin-offline'
